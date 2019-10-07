3e143b83bc18ff49d91296c0e7d137decbacd562
Xavi Torelló <info@xaviertorello.cat>
2019-10-07 14:06:41 +0200

    Initialize base SchedTalks

--- sched_talks.py
+++ sched_talks.py
@@ -1,2 +1,11 @@
 class SchedTalks(object):
+    def __init__(self, **kwargs):
+        """Load settings"""
+
+        self.settings = {
+            'output_dir': 'files',
+            **kwargs,
+        }
+
+        self.talks = []
     pass

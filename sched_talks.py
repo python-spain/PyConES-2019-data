e53d5b258fcd18d126120bea1da2ec02dd295692
Xavi Torelló <info@xaviertorello.cat>
2019-10-07 14:12:47 +0200

    Add as_md property skipping non real talks

--- sched_talks.py
+++ sched_talks.py
@@ -24,6 +24,38 @@ class SchedTalks(object):
         self._get_talks()
 
     @property
+    def as_md(self):
+        """
+        Render talks as a MD string
+
+        It skips non real talks i.e Coffe breaks, lunchs and other similar entries 
+        based on the speaker definition.
+        """
+
+        talks_content = ''
+        for talk in tqdm(self.talks, desc='Converting to MD'):
+            speakers = [speaker.get('name', 'Unknown')
+                        for speaker in talk.get('speakers', [])]
+
+            # Skip unnamed talks (coffee break, lunch, ...)
+            if not speakers:
+                continue
+
+            speakers_formated = ", ".join(speakers)
+            talk_md = f"\n- **'{talk.get('name')}'** by _{speakers_formated}_"
+
+            attachments = talk.get('attachments')
+            if attachments:
+                talk_md += ''.join([
+                    f'\n  - [{attachment.get("file_name")}]({attachment.get("file_path")})'
+                    for attachment in attachments
+                ])
+            
+            talks_content += talk_md
+
+        return talks_content
+     
+    @property
     def output_dir(self):
         return self.settings.get('output_dir')
 
